14e5ba1219e9b0229a26366699f38c29b60026fe
janelle mellor <janellemellor@gmail.com>
2020-04-30 16:00:27 -0700

    add handler prop to input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Form = ({ onSubmit, url, onUrlChange, method, response, onResponseChange }) => (
+const Form = ({ onSubmit, url, onUrlChange, method, onInputChange, response, onResponseChange }) => (
   <form onSubmit={onSubmit}>
     <fieldset>
       <input name="url-input" type="text" value={url} onUrlChange={onUrlChange} />
       <label> GET
-        <input type="radio" name="methods" value={method}/>
+        <input type="radio" name="methods" value={method} onInputChange={onInputChange}/>
       </label>
       <label> POST
-        <input type="radio" name="methods" value={method}/>
+        <input type="radio" name="methods" value={method} onInputChange={onInputChange}/>
       </label>  
       <label> PUT
-        <input type="radio" name="methods" value={method}/>
+        <input type="radio" name="methods" value={method} onInputChange={onInputChange}/>
       </label>  
       <label> PATCH
-        <input type="radio" name="methods" value={method}/>
+        <input type="radio" name="methods" value={method} onInputChange={onInputChange}/>
       </label>  
       <label> DELETE
-        <input type="radio" name="methods" value={method}/>
+        <input type="radio" name="methods" value={method} onInputChange={onInputChange}/>
       </label>    
       <button>Go!</button>
       <label>
@@ -33,6 +33,7 @@ Form.propTypes = {
   url: PropTypes.string.isRequired,
   onUrlChange: PropTypes.func.isRequired,
   method: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
   response: PropTypes.string.isRequired,
   onResponseChange: PropTypes.func.isRequired
 };
