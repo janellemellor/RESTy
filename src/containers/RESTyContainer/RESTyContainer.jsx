128225d5b847fe9d43a923a8e862fa4e6a34e498
janelle mellor <janellemellor@gmail.com>
2020-05-09 11:40:36 -0700

    add local storage to container and update handle submit

--- src/containers/RESTyContainer/RESTyContainer.jsx
+++ src/containers/RESTyContainer/RESTyContainer.jsx
@@ -3,13 +3,14 @@ import Form from '../../components/Form/Form.jsx';
 import Response from '../../components/Response/Response.jsx';
 import History from '../../components/History/History.jsx';
 import { fetchRequest } from '../../services/fetchRequest.js';
+import { useLocalStorage } from '../../hooks/localStorage.js';
 
 const RESTyContainer = () => {
   const [url, setUrl] = useState('');
   const [method, setMethod] = useState('GET');
   const [jsonBody, setJsonBody] = useState('');
   const [response, setResponse] = useState({});
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useLocalStorage('history', []);
   
   const handleChange = ({ target }) => {
     if(target.name === 'url-input') setUrl(target.value);
@@ -22,7 +23,8 @@ const RESTyContainer = () => {
     fetchRequest(url, method, jsonBody)
       .then(res => {
         setResponse(res);
-      }); console.log(response);
+        setHistory(prevHistory => [{ url, method }, ...prevHistory]);
+      }); 
   };
   
   return (
