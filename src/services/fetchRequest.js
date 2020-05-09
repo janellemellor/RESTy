57cce54233032bcb3a38ca3111e7fbf7ee34ed1b
janelle mellor <janellemellor@gmail.com>
2020-05-08 17:54:42 -0700

    update fetch request with ternery for headers

--- src/services/fetchRequest.js
+++ src/services/fetchRequest.js
@@ -3,13 +3,11 @@ const BODY_METHODS = ['POST', 'PUT', 'PATCH'];
 export const fetchRequest = (url, method = 'GET', body) => {
   return fetch(url, {
     method: method,
-    // body: body ? JSON.stringify(body) : null
     body: BODY_METHODS.includes(method) ? body : null,
     header: {
-      'Content-Type': BODY_METHODS.includes(method) && 'application/json'
+      'Content-Type': BODY_METHODS.includes(method) ? 'application/json' : null
     }
   }) 
     .then(res => res.json());
-  //may need to add additional shaping here.
 };
 
